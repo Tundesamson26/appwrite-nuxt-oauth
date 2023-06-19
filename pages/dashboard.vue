2e3521499bced334e46947aeba35a41976a89c3b
Tunde Samson <odewoletundesamson@gmail.com>
2023-06-19 22:15:48 +0100

    initial commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div >
+      <h2 class="u-text-center u-font-size-32 u-margin-32">
+         Amazon Authentication with Appwrite
+      </h2>
+      <div class="card u-text-center u-cross-center u-width-full-line u-max-width-350" style="margin: auto;">
+        <div>
+          <h3 class="body-text-2 u-margin-block-start-16">
+            Name: <span>{{}}</span>
+          </h3>
+          <p class="u-margin-block-start-4 u-color-text-gray">
+            Email: <span>{{}}</span>
+          </p>
+        </div>
+        <button class="button u-margin-inline-auto u-margin-block-start-24">
+          <span class="text">Logout</span>
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="u-text-center">
+    <h2 class="u-text-center u-font-size-32 u-margin-32">Login with Amazon</h2>
+    <div class="u-text-center">
+      <p>Click on this button to login</p>
+      <button
+        class="button u-margin-inline-auto u-margin-block-start-24"
+        @click="loginWithAmazon"
+      >
+        <span class="text">Login with Amazon</span>
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+import "@appwrite.io/pink"; // optionally, add icons
+import "@appwrite.io/pink-icons";
+import { Client, Account } from "appwrite";
+
+const client = new Client();
+const account = new Account(client);
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("648b874f54b5092cdf00");
+
+const loginWithAmazon = async () => {
+  try {
+    await account.createOAuth2Session(
+      "amazon",
+      "http://localhost:3000/dashboard"
+    );
+  } catch (e) {
+    alert("user does not exist");
+    console.log(e);
+  }
+};
+</script>
