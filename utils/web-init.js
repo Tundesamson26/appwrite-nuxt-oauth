9f7c0887df696ce6233bb22b07a1f4e44fd27fa8
Tunde Samson <odewoletundesamson@gmail.com>
2023-07-05 14:09:07 +0100

    updated commit

--- utils/web-init.js
+++ utils/web-init.js
@@ -1,6 +1,7 @@
 import { Client, Account } from "appwrite";
+
 export const client = new Client();
 export const account = new Account(client);
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("643fee63b2b5b506495c");
+  .setProject("648b874f54b5092cdf00");
